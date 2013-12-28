f68f1048b7168734e9ba0e15a2f6ebdfa7b9014c
basecss <i@basecss.net>
2013-12-28 14:47:44 +0800

    update file

--- daily/js-inheritance.js
+++ daily/js-inheritance.js
@@ -19,9 +19,18 @@ Employee.prototype.info = function() {
 var person = new Person('basecss');
 var basecss = new Employee('basecss', 22);
 
-// relationship
+// 原型关系
 /*
  * person [[Prototype]] -> Person.prototype
  * basecss [[Prototype]] -> Employee.prototype
  * Employee.prototype [[Prototype]] -> Person.prototype
- */ 
+ */
+
+// prototype 检查
+person instanceof Person === Person.prototype.isPrototypeOf(person);
+basecss instanceof Employee === Employee.prototype.isPrototypeOf(basecss);
+
+// 借用父类方法
+// Person.prototype.info.call(this)
+Array.prototype.slice.call(arguments);
+
